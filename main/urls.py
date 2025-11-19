4a1edd46194ab3e18289ef6459cf0617a4f4a413
shinyunseo <shinpark5523@gmail.com>
2025-11-19 23:39:18 +0900

    delete wrong

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,2 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('signup/', views.signup, name = 'signup'),
-  path('login/', views.login, name = 'login'),
-
-]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
-from .forms import SignupForm
 
 # Create your views here.
